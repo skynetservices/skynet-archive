aeeb1f86b95b6485a0ada3c57c3e4d88c6e65bc2
Christopher Dunn <cdunn2001@gmail.com>
2011-08-11 01:21:16 -0700

    rand-init can now take an arg: number of calls

--- examples/GetWidgets/initiators/rand-init/main.go
+++ examples/GetWidgets/initiators/rand-init/main.go
@@ -5,13 +5,14 @@ import (
 	"rand"
 	"time"
 	"flag"
+	"strconv"
 )
 
 func init() {
 	rand.Seed(time.Seconds())
 }
 
-func callRand(){
+func callRand() {
 	service := "MyRandomService"
 	client, _ := skylib.GetRandomClientByService(service)
 	request := rand.Intn(10) + 1
@@ -22,14 +23,23 @@ func callRand(){
 	println("Done.")
 }
 
+func run(n uint) {
+	// Ordinarily, we would use the same Service everytime, but
+	// here we are just testing.
+	for ; n > 0; n-- {
+		callRand()
+	}
+}
+
 func main() {
 
 	// These two lines are the only lines required by an initiator
 	flag.Parse()
 	skylib.NewAgent().Start()
 
-	for x:=1; x<100; x++{
-		callRand()
+	var n uint = 1
+	if flag.NArg() > 0 {
+		n, _ = strconv.Atoui(flag.Arg(0))
 	}
-
+	run(n)
 }
