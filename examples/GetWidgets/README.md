332e47a0a3c9423d84994c32608f0e81d3feeb12
Brian Ketelsen <bketelsen@gmail.com>
2012-05-15 10:23:37 -0400

    removed out of date examples and generators

--- examples/GetWidgets/README.md
+++
@@ -1,7 +0,0 @@
-Getwidgets is an example app mesh in Skynet.  
-
-To get going:
-* Build all apps - we use go-gb
-* Start Doozer
-* Start one of each app
-* Profit
--- skygen/skygen.go
+++ skygen/skygen.go
@@ -14,14 +14,6 @@ import (
 	"template"
 )
 
-
-// Service generation flags
-var excludeInitiator *bool = flag.Bool("excludeInitiator", false, "don't generate an initiator")
-var excludeRouter *bool = flag.Bool("excludeRouter", false, "don't generate a router")
-var excludeService *bool = flag.Bool("excludeService", false, "don't generate a service")
-var excludeWatcher *bool = flag.Bool("excludeWatcher", false, "don't generate a watcher")
-var excludeReaper *bool = flag.Bool("excludeReaper", false, "don't generate a reaper")
-
 // Configuration flags
 var PackageName *string = flag.String("packageName", "myCompany", "namespace of the Go package to generate")
 var ServiceName *string = flag.String("serviceName", "GetUsers", "API function to be provided by SkyNet")
@@ -33,181 +25,15 @@ func printToDo() {
 	})
 }
 
-func generateLibrary() {
-	//mkdir for the packagename
-	err := os.MkdirAll(*TargetFullPath+"/"+*PackageName, 0755)
-	if err != nil {
-		fmt.Println("Unable to create directory, ", PackageName, err.String())
-	}
-	f, err := os.Create(*TargetFullPath + "/" + *PackageName + "/package.go")
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	defer f.Close()
-	var templ *template.Template
-	templ = template.New(nil)
-	templ.SetDelims("<%", "%>")
-	err = templ.Parse(packageTemplate)
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	err = templ.Execute(f, map[string]interface{}{
-		"PackageName": *PackageName,
-		"ServiceName": *ServiceName,
-	})
-
-	if err != nil {
-		fmt.Println(err.String())
-	}
-}
-
-func generateInitiator() {
-	//mkdir for the initiator
-	err := os.MkdirAll(*TargetFullPath+"/initiators/web/", 0755)
-	if err != nil {
-		fmt.Println("Unable to create directory, ", err.String())
-	}
-	f, err := os.Create(*TargetFullPath + "/initiators/web/" + "web.go")
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	defer f.Close()
-	var templ *template.Template
-	templ = template.New(nil)
-	templ.SetDelims("<%", "%>")
-	err = templ.Parse(initiatorTemplate)
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	err = templ.Execute(f, map[string]interface{}{
-		"PackageName": *PackageName,
-		"ServiceName": *ServiceName,
-	})
-
-	if err != nil {
-		fmt.Println(err.String())
-	}
-}
-
-func generateRouter() {
-	//mkdir for the initiator
-	err := os.MkdirAll(*TargetFullPath+"/router/", 0755)
-	if err != nil {
-		fmt.Println("Unable to create directory, ", err.String())
-	}
-	f, err := os.Create(*TargetFullPath + "/router/" + "router.go")
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	defer f.Close()
-	var templ *template.Template
-	templ = template.New(nil)
-	templ.SetDelims("<%", "%>")
-	err = templ.Parse(routerTemplate)
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	err = templ.Execute(f, map[string]interface{}{
-		"PackageName": *PackageName,
-		"ServiceName": *ServiceName,
-	})
-
-	if err != nil {
-		fmt.Println(err.String())
-	}
-}
-
 func generateService() {
-	//mkdir for the initiator
-	err := os.MkdirAll(*TargetFullPath+"/service/", 0755)
-	if err != nil {
-		fmt.Println("Unable to create directory, ", err.String())
-	}
-	f, err := os.Create(*TargetFullPath + "/service/" + "service.go")
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	defer f.Close()
-	var templ *template.Template
-	templ = template.New(nil)
-	templ.SetDelims("<%", "%>")
-	err = templ.Parse(serviceTemplate)
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	err = templ.Execute(f, map[string]interface{}{
-		"PackageName": *PackageName,
-		"ServiceName": *ServiceName,
-	})
-
-	if err != nil {
-		fmt.Println(err.String())
-	}
+	//TODO
 }
 
-func generateReaper() {
-	//mkdir for the initiator
-	err := os.MkdirAll(*TargetFullPath+"/watchers/reaper/", 0755)
-	if err != nil {
-		fmt.Println("Unable to create directory, ", err.String())
-	}
-	f, err := os.Create(*TargetFullPath + "/watchers/reaper/" + "reaper.go")
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	defer f.Close()
-	var templ *template.Template
-	templ = template.New(nil)
-	templ.SetDelims("<%", "%>")
-	err = templ.Parse(reaperTemplate)
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	err = templ.Execute(f, map[string]interface{}{
-		"PackageName": *PackageName,
-		"ServiceName": *ServiceName,
-	})
-
-	if err != nil {
-		fmt.Println(err.String())
-	}
-}
-func generateWatcher() {
-	//mkdir for the initiator
-	err := os.MkdirAll(*TargetFullPath+"/watchers/generic/", 0755)
-	if err != nil {
-		fmt.Println("Unable to create directory, ", err.String())
-	}
-	f, err := os.Create(*TargetFullPath + "/watchers/generic/" + "generic.go")
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	defer f.Close()
-	var templ *template.Template
-	templ = template.New(nil)
-	templ.SetDelims("<%", "%>")
-	err = templ.Parse(watcherTemplate)
-	if err != nil {
-		fmt.Println(err.String())
-	}
-	err = templ.Execute(f, map[string]interface{}{
-		"PackageName": *PackageName,
-		"ServiceName": *ServiceName,
-	})
-
-	if err != nil {
-		fmt.Println(err.String())
-	}
-}
 //Skynet Generator creates the files necessary for a skynet installation
 // Minimum required are an Initiator and a Service
 //
 //
-// flags: -excludeInitiator
-//		  -excludeRouter
-//        -excludeService
-//        -excludeWatcher
-//        -excludeReaper
+// flags: 
 //        -packageName=myPackage
 //        -serviceName=GetUsers
 // 
@@ -215,22 +41,6 @@ func main() {
 	flag.Parse()
 
 	printToDo()
+	generateService()
 
-	generateLibrary()
-	
-	if !*excludeInitiator {
-		generateInitiator()
-	}
-	if !*excludeRouter {
-		generateRouter()
-	}
-	if !*excludeReaper {
-		generateReaper()
-	}
-	if !*excludeService {
-		generateService()
-	}
-	if !*excludeWatcher {
-		generateWatcher()
-	}
 }
