0c24c57233ffda5cd34439acc1d81b35340c74c3
Erik St. Martin <alakriti@gmail.com>
2012-06-14 11:42:35 -0400

    bash script to build/test entire library

---
+++ all.sh
@@ -0,0 +1,14 @@
+# Build / Test skylib
+cd skylib && go build && go test
+cd ..
+
+# Build / Test sky
+cd sky && go build && go test
+cd ..
+
+# Build / Test examples
+cd examples/client && go build && go test
+cd ../../
+
+cd examples/service && go build && go test
+cd ../../
