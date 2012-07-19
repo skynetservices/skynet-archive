fd91f0ee514f126dd2804e4f7bb118ad39b84b73
John Asmuth <jasmuth@gmail.com>
2012-07-19 10:22:30 -0400

    moved handshake definitions into own file

--- skylib/service.go
+++ skylib/service.go
@@ -49,18 +49,6 @@ type Service struct {
 	rpcListener *net.TCPListener `json:"-"`
 }
 
-// ServiceHandshake is data sent by the service to the client immediately once the connection
-// is opened.
-type ServiceHandshake struct {
-	// Registered indicates the state of this service. If it is false, the connection will
-	// close immediately and the client should look elsewhere for this service.
-	Registered bool
-}
-
-// ClientHandshake is sent by the client to the service after receipt of the ServiceHandshake.
-type ClientHandshake struct {
-}
-
 func CreateService(s ServiceDelegate, c *ServiceConfig) *Service {
 	// This will set defaults
 	initializeConfig(c)
