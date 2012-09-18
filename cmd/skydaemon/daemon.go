7d0168c3d80996a2ee37224f089fc5665f6357d0
Erik St. Martin <alakriti@gmail.com>
2012-09-18 09:19:47 -0400

    removing Start logic from daemon, as start is part of deploy, and the daemon won't hold any sort of config of known services

--- cmd/skydaemon/daemon.go
+++ cmd/skydaemon/daemon.go
@@ -103,44 +103,6 @@ func (s *SkynetDaemon) StopAllSubServices(requestInfo *skynet.RequestInfo, in da
 	return
 }
 
-func (s *SkynetDaemon) StartAllSubServices(requestInfo *skynet.RequestInfo, in daemon.StartAllSubServicesRequest, out *daemon.StartAllSubServicesResponse) (err error) {
-	var uuids []string
-	s.serviceLock.Lock()
-	for uuid := range s.Services {
-		uuids = append(uuids, uuid)
-	}
-	s.serviceLock.Unlock()
-
-	if len(uuids) == 0 {
-		err = errors.New("No services deployed")
-		return
-	}
-
-	out.Starts = make([]daemon.StartSubServiceResponse, len(uuids))
-
-	for i, uuid := range uuids {
-		err = s.StartSubService(requestInfo, daemon.StartSubServiceRequest{UUID: uuid}, &out.Starts[i])
-		if err != nil {
-			return
-		}
-		if out.Starts[i].Ok {
-			out.Count++
-		}
-	}
-	return
-}
-
-func (s *SkynetDaemon) StartSubService(requestInfo *skynet.RequestInfo, in daemon.StartSubServiceRequest, out *daemon.StartSubServiceResponse) (err error) {
-	ss := s.getSubService(in.UUID)
-	if ss != nil {
-		out.Ok, _ = ss.Start()
-		out.UUID = in.UUID
-	} else {
-		err = errors.New(fmt.Sprintf("No such service UUID %q", in.UUID))
-	}
-	return
-}
-
 func (s *SkynetDaemon) StopSubService(requestInfo *skynet.RequestInfo, in daemon.StopSubServiceRequest, out *daemon.StopSubServiceResponse) (err error) {
 	ss := s.getSubService(in.UUID)
 	if ss != nil {
