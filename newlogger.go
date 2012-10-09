3fcaa0d47b0584efd0bdf6ced9a7966c9c445f89
Steve Phillips <elimisteve@gmail.com>
2012-10-09 09:12:52 -0700

    Added Payload and Exception types

--- newlogger.go
+++ newlogger.go
@@ -4,19 +4,23 @@ import (
 	"time"
 )
 
-type LogPayload struct {
-	Duration time.Duration `json:"duration"`
-	Result   interface{}   `json:"result"` // What type should this be?
-	Table    string        `json:"table"`
-	Action   string        `json:"action"`
+type Exception struct {
+	Exception string `json:"exception"`
+	Message   string `json:"message"`
+	Backtrace string `json:"backtrace"`
 }
 
-// Will rename to just 'Logger' once transition is complete
-type MyLogger interface {
-	Trace(msg string, payload ...*LogPayload)
-	Debug(msg string, payload ...*LogPayload)
-	Info(msg string, payload ...*LogPayload)
-	Warn(msg string, payload ...*LogPayload)
-	Error(msg string, payload ...*LogPayload)
-	Fatal(msg string, payload ...*LogPayload)
+type Payload struct {
+	Name        string        `json:"name"`
+	Application string        `json:"application"`
+	HostName    string        `json:"host_name"`
+	ThreadName  string        `json:"thread_name"`
+	Message     string        `json:"message"`
+	Tags        []string      `json:"tags"`
+	PID         int64         `json:"pid"`
+	Level       string        `json:"level"`
+	Time        time.Time     `json:"time"`
+	Duration    time.Duration `json:"duration"`
+	Table       string        `json:"table"`
+	Action      string        `json:"action"`
 }
