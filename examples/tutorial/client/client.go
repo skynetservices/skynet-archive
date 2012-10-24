b7d4b7453b12aa0cedb2e92de37801920d4b876e
Erik St. Martin <alakriti@gmail.com>
2012-10-24 10:17:40 -0400

    go fmt on tutorial/

--- examples/tutorial/client/client.go
+++ examples/tutorial/client/client.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "github.com/bketelsen/skynet"
-  "github.com/bketelsen/skynet/client"
-  "fmt"
+	"fmt"
+	"github.com/bketelsen/skynet"
+	"github.com/bketelsen/skynet/client"
 )
 
 type TutorialRequest struct {
-  Value int
+	Value int
 }
 
 type TutorialResponse struct {
-  Value int
+	Value int
 }
 
-func main(){
+func main() {
 	config, _ := skynet.GetClientConfig()
 	client := client.NewClient(config)
 
 	service := client.GetService("TutorialService", "1", "Development", "")
 
-  req := &TutorialRequest {
-    Value: 1,
-  }
+	req := &TutorialRequest{
+		Value: 1,
+	}
 
-  resp := &TutorialResponse{}
+	resp := &TutorialResponse{}
 
-  err := service.Send(nil, "AddOne", req, resp)
+	err := service.Send(nil, "AddOne", req, resp)
 
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    fmt.Println(resp.Value)
-  }
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resp.Value)
+	}
 }
