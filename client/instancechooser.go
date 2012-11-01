33f07db596ad67a864d20313363562c5a3f49fdc
John Asmuth <jasmuth@gmail.com>
2012-11-01 14:56:46 -0400

    heapsort instances before choosing them

--- client/instancechooser.go
+++ client/instancechooser.go
@@ -1,11 +1,31 @@
 package client
 
 import (
+	"container/heap"
 	"github.com/bketelsen/skynet"
-	//"math/rand"
 )
 
+type InstanceComparator func(c *Client, i1, i2 *skynet.ServiceInfo) (i1IsLess bool)
+
+func basicComparator(c *Client, i1, i2 *skynet.ServiceInfo) (i1IsBetter bool) {
+	region := c.Config.Region
+	// if only one has the right region, it's definitely less
+	if region == i1.Config.Region && region != i2.Config.Region {
+		return true
+	}
+	if region != i1.Config.Region && region == i2.Config.Region {
+		return false
+	}
+	// otherwise use something arbitrary
+	return i1.Config.UUID < i2.Config.UUID
+}
+
 type InstanceChooser struct {
+	// client is whom we are choosing instances for
+	client *Client
+
+	comparator InstanceComparator
+
 	instances []*skynet.ServiceInfo
 	count     int
 
@@ -14,11 +34,19 @@ type InstanceChooser struct {
 	chooseCh chan chan *skynet.ServiceInfo
 }
 
-func NewInstanceChooser() (ic *InstanceChooser) {
+func NewInstanceChooser(c *Client) (ic *InstanceChooser) {
 	ic = &InstanceChooser{
-		addCh:    make(chan *skynet.ServiceInfo, 1),
-		remCh:    make(chan *skynet.ServiceInfo, 1),
-		chooseCh: make(chan chan *skynet.ServiceInfo),
+		client:     c,
+		comparator: basicComparator,
+		addCh:      make(chan *skynet.ServiceInfo, 1),
+		remCh:      make(chan *skynet.ServiceInfo, 1),
+		chooseCh:   make(chan chan *skynet.ServiceInfo),
+	}
+
+	if c.Config.Prioritizer != nil {
+		ic.comparator = func(c *Client, i1, i2 *skynet.ServiceInfo) (i1IsBetter bool) {
+			return c.Config.Prioritizer(i1, i2)
+		}
 	}
 
 	go ic.mux()
@@ -88,8 +116,41 @@ func (ic *InstanceChooser) Choose(timeout chan bool) (instance *skynet.ServiceIn
 }
 
 func (ic *InstanceChooser) choose() (instance *skynet.ServiceInfo) {
-	i := ic.count % len(ic.instances) //rand.Intn(len(ic.instances))
-	instance = ic.instances[i]
-	ic.count++
+	// if the client's region is unknown, choose randomly
+	if ic.client.Config.Region == skynet.DefaultRegion {
+		i := ic.count % len(ic.instances) //rand.Intn(len(ic.instances))
+		instance = ic.instances[i]
+		ic.count++
+		return
+	}
+
+	// otherwise, choose services in the same region
+
+	// this heap-sorts (in linear time) the instances according to ic.comparator
+	heap.Init((*InstanceHeap)(ic))
+	instance = ic.instances[0]
 	return
+}
+
+type InstanceHeap InstanceChooser
+
+func (h *InstanceHeap) Len() int {
+	return len(h.instances)
+}
+
+func (h *InstanceHeap) Less(i, j int) bool {
+	// the indices are reversed here on purpose. if i<j, it goes at the end of the list rather than the beginning.
+	return h.comparator(h.client, h.instances[j], h.instances[i])
+}
+
+func (h *InstanceHeap) Swap(i, j int) {
+	h.instances[i], h.instances[j] = h.instances[j], h.instances[i]
+}
+
+func (h *InstanceHeap) Push(x interface{}) {
+	panic("invalid use")
+}
+
+func (h *InstanceHeap) Pop() interface{} {
+	panic("invalid use")
 }
