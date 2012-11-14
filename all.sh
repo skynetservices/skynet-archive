72ea4d62f035f20eb1f4661b41bfbb7a8b467de6
Erik St. Martin <alakriti@gmail.com>
2012-11-14 14:10:04 -0500

    updating all.sh to build and all libs and example services/clients to ensure they all build and tests pass when changes are made

--- all.sh
+++ all.sh
@@ -1,13 +1,62 @@
 # Build / Test skylib
 go build && go test
 
-# Build / Test sky
-cd sky && go build && go test
+# Build Client
+cd client && go build && go test
 cd ..
+
+# Build Service
+cd service && go build && go test
+cd ..
+
+# Build Daemon
+cd daemon && go build && go test
+cd ..
+
+# Build Pools
+cd pools && go build && go test
+cd ..
+
+# Build RPC
+cd rpc/bsonrpc && go build && go test
+cd ../../
+
+# Build / Test sky
+cd cmd/sky && go build && go test
+cd ..
+
+# Build / Test dashboard
+cd dashboard && go build && go test
+cd ..
+
+# Build / Test skydaemon
+cd skydaemon && go build && go test
+cd ../../
 
 # Build / Test examples
 cd examples/client && go build && go test
+cd ../
+
+cd service && go build && go test
+cd ../
+
+cd tutorial/client && go build && go test
+cd ../
+
+cd service && go build && go test
 cd ../../
 
-cd examples/service && go build && go test
+cd testing/fibonacci/fibclient && go build && go test
+cd ../
+
+cd fibservice && go build && go test
 cd ../../
+
+cd sleeper/sleepclient && go build && go test
+cd ../
+
+cd sleepservice && go build && go test
+cd ../../
+
+cd vagranttests && go build && go test
+cd ../../../
