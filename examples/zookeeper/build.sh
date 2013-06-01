20b0f8cc5c86c17937fe8b6e4553062b9231837a
Erik St. Martin <alakriti@gmail.com>
2013-05-31 20:20:18 -0400

    removing binary, and unneeded build script as we just need to export the CGO flags

--- examples/zookeeper/build.sh
+++
@@ -1,2 +0,0 @@
-root=/Users/erik/go/src/github.com/skynetservices/skynet/examples
-CGO_CFLAGS="-I$root/tools/starter-kit-osx/zookeeper/include" CGO_LDFLAGS="$root/tools/starter-kit-osx/zookeeper/lib/libzookeeper_mt.a" go build
