f61eb34b82ddf00bbe0a57f60ff83fe850507d73
Erik St. Martin <alakriti@gmail.com>
2013-06-05 12:31:02 -0400

    sky - organizing ssh logic a bit better

--- cmd/sky/build.go
+++ cmd/sky/build.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"code.google.com/p/go.crypto/ssh"
-	"code.google.com/p/gopass"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -26,7 +24,7 @@ type build struct {
 	PreBuildCommands  []string
 	PostBuildCommands []string
 
-	sshClient *ssh.ClientConn
+	sshClient *SSHConn
 }
 
 func Build() {
@@ -45,7 +43,8 @@ func Build() {
 		fmt.Println("Failed to parse build.cfg: " + err.Error())
 	}
 
-	b.connect()
+	b.sshClient = new(SSHConn)
+	b.sshClient.Connect(b.Host, b.User)
 	defer b.sshClient.Close()
 
 	b.perform()
@@ -57,64 +56,26 @@ func (b *build) perform() {
 
 func (b *build) validate() {
 	// Validate Jail exists
-	out, err := b.sshExec("ls " + b.Jail)
+	_, err := b.sshClient.Exec("ls " + b.Jail)
 	if err != nil {
 		fmt.Println("Could not find Jail directory: " + err.Error())
 	}
 
 	// Validate GOROOT exists
-	out, err = b.sshExec("ls " + b.GoRoot)
+	_, err = b.sshClient.Exec("ls " + b.GoRoot)
 	if err != nil {
 		fmt.Println("Could not find GOROOT directory: " + err.Error())
 	}
 
 	// Validate Go Binary exists
-	out, err = b.sshExec("ls " + b.GoRoot + "/bin/go")
+	_, err = b.sshClient.Exec("ls " + b.GoRoot + "/bin/go")
 	if err != nil {
 		fmt.Println("Could not find Go binary: " + err.Error())
 	}
 
 	// Validate Git exists
-	out, err = b.sshExec("which git")
+	_, err = b.sshClient.Exec("which git")
 	if err != nil {
 		fmt.Println("Could not find Git binary: " + err.Error())
 	}
-}
-
-func (b build) Password(user string) (string, error) {
-	pass, err := gopass.GetPass("Password for " + user + ": ")
-
-	if err != nil {
-		panic("Failed to collect password: " + err.Error())
-	}
-
-	return pass, err
-}
-
-// SSH logic
-func (b *build) connect() {
-	config := &ssh.ClientConfig{
-		User: b.User,
-		Auth: []ssh.ClientAuth{
-			ssh.ClientAuthPassword(b),
-		},
-	}
-
-	var err error
-	b.sshClient, err = ssh.Dial("tcp", b.Host, config)
-	if err != nil {
-		panic("Failed to dial: " + err.Error())
-	}
-}
-
-func (b *build) sshExec(cmd string) (out []byte, err error) {
-	var session *ssh.Session
-
-	session, err = b.sshClient.NewSession()
-	if err != nil {
-		panic("Failed to create session: " + err.Error())
-	}
-	defer session.Close()
-
-	return session.CombinedOutput(cmd)
 }
---
+++ cmd/sky/ssh.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"code.google.com/p/go.crypto/ssh"
+	"code.google.com/p/gopass"
+	"errors"
+)
+
+type SSHConn struct {
+	host   string
+	user   string
+	client *ssh.ClientConn
+}
+
+// SSH logic
+func (c *SSHConn) Connect(host, user string) error {
+	c.host = host
+	c.user = user
+
+	config := &ssh.ClientConfig{
+		User: c.user,
+		Auth: []ssh.ClientAuth{
+			ssh.ClientAuthPassword(c),
+		},
+	}
+
+	var err error
+	c.client, err = ssh.Dial("tcp", c.host, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *SSHConn) Exec(cmd string) (out []byte, err error) {
+	var session *ssh.Session
+
+	session, err = c.client.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	defer session.Close()
+
+	return session.CombinedOutput(cmd)
+}
+
+func (c *SSHConn) Password(user string) (string, error) {
+	pass, err := gopass.GetPass("Password for " + user + ": ")
+
+	if err != nil {
+		return "", errors.New("Failed to collect password: " + err.Error())
+	}
+
+	return pass, err
+}
+
+func (c *SSHConn) Close() {
+	c.client.Close()
+}
