3b4d78f8978fe1b6d5df96d33976f081211da920
Erik St. Martin <alakriti@gmail.com>
2013-06-10 15:38:39 -0400

    removing config logic for daemon as it's going to be implemented differently

--- cmd/skydaemon/main.go
+++ cmd/skydaemon/main.go
@@ -16,9 +16,6 @@ import (
 
 // Daemon will run and maintain skynet services.
 //
-// Daemon will initially start those specified in the file given in
-// the "-config" option
-//
 // Daemon will run the "SkynetDeployment" service, which can be used
 // to remotely spawn new services on the host.
 func main() {
@@ -45,13 +42,6 @@ func main() {
 		}
 	}()
 
-	if len(args) == 1 {
-		err := deployConfig(deployment, args[0])
-		if err != nil {
-			log.Println(log.ERROR, err.Error())
-		}
-	}
-
 	// Collect Host metrics
 	statTicker := time.Tick((5 * time.Second))
 	go func() {
@@ -63,42 +53,4 @@ func main() {
 	// If we pass false here service will not be Registered
 	// we could do other work/tasks by implementing the Started method and calling Register() when we're ready
 	s.Start(true).Wait()
-}
-
-// deploy each of the services listed in the provided file
-func deployConfig(s *SkynetDaemon, cfg string) (err error) {
-	cfgFile, err := os.Open(cfg)
-	if err != nil {
-		return
-	}
-	br := bufio.NewReader(cfgFile)
-	for {
-		var bline []byte
-		var prefix bool
-		bline, prefix, err = br.ReadLine()
-		if err == io.EOF {
-			err = nil
-			break
-		}
-		if err != nil {
-			return
-		}
-		if prefix {
-			err = errors.New("Config line to long in " + cfg)
-			return
-		}
-		line := strings.TrimSpace(string(bline))
-		if len(line) == 0 {
-			continue
-		}
-
-		split := strings.Index(line, " ")
-		if split == -1 {
-			split = len(line)
-		}
-		servicePath := line[:split]
-		args := strings.TrimSpace(line[split:])
-		s.Start(&skynet.RequestInfo{}, daemon.StartRequest{ServicePath: servicePath, Args: args}, &daemon.StartResponse{})
-	}
-	return
 }
