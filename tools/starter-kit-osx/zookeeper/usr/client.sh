c0cff88beb9acddea26a82cadd89f7f21e23928a
Erik St. Martin <alakriti@gmail.com>
2013-08-22 15:54:25 -0400

    removing zookeeper from tools, it's no longer needed to compile, and to run the zookeeper service it's easiest to just install it via package manager apt/yum/brew

--- tools/starter-kit-osx/zookeeper/usr/client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-base_dir=$(dirname $0)
-for file in $base_dir/libs/*.jar;
-do
-  CLASSPATH=$CLASSPATH:$file
-done
-CLASSPATH=$CLASSPATH:config
-java -cp $CLASSPATH \
-	org.apache.zookeeper.ZooKeeperMain -server 127.0.0.1:2181
